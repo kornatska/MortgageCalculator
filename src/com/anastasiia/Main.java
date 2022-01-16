58c59da4984f2ea69d4169ebe00acbdb7128215f
kornatska <anastasiia.kornatska@gmail.com>
2022-01-16 14:48:09 -0800

    mortgage calculator

--- src/com/anastasiia/Main.java
+++ src/com/anastasiia/Main.java
@@ -8,14 +8,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Principal: ");
-        String principal = scanner.nextLine();
-        int p = Integer.parseInt(principal);
+        int p = scanner.nextInt();
         System.out.print("Annual Interest Rate: ");
-        String interest = scanner.nextLine();
-        double i = Double.parseDouble(interest);
+        double i = scanner.nextDouble();
         System.out.print("Period (Years): ");
-        String period = scanner.nextLine();
-        int y = Integer.parseInt(period);
+        int y = scanner.nextInt();
         double r = i / 12 / 100;
         int n = y * 12;
         double m = p * r * Math.pow((1 + r), n) / (Math.pow((1 + r), n) - 1);
@@ -24,15 +21,3 @@ public class Main {
         System.out.println("Mortgage: " + mort);
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
