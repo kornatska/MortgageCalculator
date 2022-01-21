6d9bbad9e6db964bedb03e29238012222bcfabbd
kornatska <anastasiia.kornatska@gmail.com>
2022-01-20 19:58:05 -0800

    mortgage calculator - hints added

--- src/com/anastasiia/Main.java
+++ src/com/anastasiia/Main.java
@@ -9,18 +9,39 @@ public class Main {
         final byte MONTHS_IN_YEAR = 12;
         final byte PERCENT = 100;
 
+        int principal;
+        float monthlyInterest;
+        int numberOfPayments;
+
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Principal: ");
-        int principal = scanner.nextInt();
+        while (true) {
+            System.out.print("Principal ($1K-$1M): ");
+            principal = scanner.nextInt();
+            if (principal >= 1000 && principal <= 1_000_000)
+                break;
+            System.out.println("Enter a number between 1,000 and 1,000,000");
+            }
 
-        System.out.print("Annual Interest Rate: ");
-        float anualInterest = scanner.nextFloat();
-        float monthlyInterest = anualInterest / MONTHS_IN_YEAR / PERCENT;
+        while (true) {
+            System.out.print("Annual Interest Rate: ");
+            float anualInterest = scanner.nextFloat();
+            if (anualInterest > 0 && anualInterest <= 30) {
+                monthlyInterest = anualInterest / MONTHS_IN_YEAR / PERCENT;
+                break;
+            }
+            System.out.println("Enter a value greater than 0 and less than or equal to 30");
+        }
 
-        System.out.print("Period (Years): ");
-        byte years = scanner.nextByte();
-        int numberOfPayments = years * MONTHS_IN_YEAR;
+        while (true) {
+            System.out.print("Period (Years): ");
+            byte years = scanner.nextByte();
+            if (years >= 1 && years <= 30) {
+                numberOfPayments = years * MONTHS_IN_YEAR;
+                break;
+            }
+            System.out.println("Enter a value between 1 and 30");
+        }
 
         double mortgage = principal
                 * monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments)
